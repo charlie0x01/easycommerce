3fefe30f03cc68bf1f99b59761b669554bbb5d30
Abdullah Tanveer <abdullahtanveer56@outlook.com>
2024-07-09 10:58:14 +0500

    config: configure sequelize for mysql database connection

---
+++ src/config/database.config.ts
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { Sequelize } from "sequelize-typescript";
+
+console.log("db username: ", process.env.USER);
+
+// database connection details
+const database: string = process.env.DATABASE || "";
+const username: string = process.env.USER || "";
+const password: string = process.env.PASSWORD || "";
+const host: string | undefined = process.env.HOST;
+
+export const connect = async (): Promise<Sequelize | undefined> => {
+  try {
+    const _connection = new Sequelize(database, username, password, {
+      host: host,
+      dialect: "mysql",
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+      },
+    });
+
+    // check database connection
+    _connection.authenticate().then(() => {
+      console.log("✅ Database Connected!!");
+    });
+
+    return _connection;
+  } catch (error) {
+    console.log("❌ ", error);
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-import {app, PORT} from "./app";
+import { app, PORT } from "./app";
+import { connect } from "./config/database.config";
+import mysql from "mysql2";
 
+// connect database
+export const database = connect();
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
