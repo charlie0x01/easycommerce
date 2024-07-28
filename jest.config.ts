59995a140239a12fcba87b35407e3851829e5728
Abdullah Tanveer <abdullahtanveer56@outlook.com>
2024-07-28 15:50:46 +0500

    tesing husky

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,13 @@
-import type {Config} from 'jest';
+import type { Config } from "jest";
 
 const config: Config = {
   clearMocks: true,
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageProvider: "v8",
-  preset: 'ts-jest',
-  testEnvironment: "jest-environment-node",
+  preset: "ts-jest",
+  testEnvironment: "node",
+  injectGlobals: true,
 };
 
 export default config;
--- tests/sample.test.ts
+++ tests/sample.test.ts
@@ -1,12 +1,15 @@
-const request = require("supertest");
-const { app } = require("../src/app");
+// import supertest from 'supertest';
+// import { app } from "../src/app";
 
-describe("Testing First Ever API", () => {
-  // server should return "Welcome to EasyCommerce!"
-  test("Get Request Test", async () => {
-    expect.assertions(2);
-    const response = await request(app).get("/").send();
-    expect(response?._body?.success).toEqual(true);
-    expect(response?.status).toEqual(200)
-  });
-});
+// import "ts-jest/globals"
+// import "jest/index"
+
+// describe("Testing First Ever API", () => {
+//   // server should return "Welcome to EasyCommerce!"
+//   test("Get Request Test", async () => {
+//     expect.assertions(2);
+//     const response = await supertest(app).get("/").send();
+//     expect(response?._body?.success).toEqual(true);
+//     expect(response?.status).toEqual(200)
+//   });
+// });
